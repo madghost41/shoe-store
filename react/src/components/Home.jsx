72265ff632467090156db96f13ef321cf3f0756b
Stephen Thomas <stephenmicheal97@gmail.com>
2025-03-20 13:37:41 -0400

    add shoecard

--- react/src/components/Home.jsx
+++ react/src/components/Home.jsx
@@ -1,16 +1,53 @@
+import React, { useState, useEffect } from "react";
 import ShoeCard from "./ShoeCard";
 
-const Home = () => {
-    return(
-        <>
-         <ShoeCard/>
-    
-        
-        </>
-    )
+const Home = (props) => {
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
 
-    
-    
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/shoes/${page}/10`
+        );
+        if (!response.ok) {
+          throw new Error("Data could not be fetched!");
+        }
+        const json_response = await response.json();
+        console.log("Fetched Shoes!", json_response);
+        setData(json_response);
+      } catch (error) {
+        console.error("Error fetching shoes:", error);
+      }
+    };
+
+    fetchData();
+  }, [page]);
+
+  const handleNextPage = () => {
+    setPage(page + 1);
+  };
+
+  const handlePreviousPage = () => {
+    setPage(page - 1);
+  };
+
+  return (
+    <>
+      <div>HOME PAGE</div>
+      <button onClick={handleNextPage}>Next</button>
+      <button onClick={handlePreviousPage}>Previous</button>
+      <div
+        className="card-container"
+        style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}
+      >
+        {data.map((shoe) => (
+          <ShoeCard key={shoe.shoeDetails.shoe_id} data={shoe.shoeDetails} />
+        ))}
+      </div>
+    </>
+  );
 };
 
-export default Home 
+export default Home;
--- react/src/components/ShoeCard.jsx
+++ react/src/components/ShoeCard.jsx
@@ -1,29 +1,48 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+;
 
-const ShoeCard = () => {
-  const [data, setData] = useState([]);
- 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://localhost:3000/shoes");
-        if (!response.ok) {
-          throw new Error('Data could not be fetched!');
-        }
-        const json_response = await response.json();
-        console.log('Fetched Shoes!', json_response)
-        setData(json_response);
-      } catch (error) {
-        console.error('Error fetching socks:', error);
-      }
-    };
+const ShoeCard = ({ data }) => {
+  // console.log("Shoe data:", data);
 
-    fetchData();
-}, []);
+
+
+//  const handleDelete = async (shoe_id) => {
+//    try {
+//      // Make an API request to delete the sock with the given sockId
+//      const response = await fetch(
+//        `${import.meta.env.VITE_MONGO_DB_URL}/${shoe_id}`,
+//        {
+//          method: "DELETE",
+//        }
+//      );
+//      if (!response.ok) {
+//        throw new Error("Shoe could not be deleted!");
+//      }
+//      // Update the state or fetch the updated data from the server
+//      const updatedData = data.filter((shoe) => sock._id !== sockId); // Remove the deleted sock from the data array
+//      setData(updatedData); // Update the state with the updated data
+//    } catch (error) {
+//      console.error("Error deleting sock:", error);
+//    }
+//  };
 
   return (
     <>
-      <div>Shoe</div>
+       <div
+        className="card card-background"
+        style={{ flex: "1", minWidth: "300px", maxWidth: "45%" }}
+      >
+        <div className="card-body">
+          <h5 className="card-title">Shoe Details</h5>
+          <div className="card-text">Size: {data.size}</div>
+          <div className="card-text">Brand: {data.brand}</div>
+          <div className="card-text">Price: ${data.price}</div>
+          <div className="card-text">Style: {data.style}</div>
+          <div className="card-text">Rating: {data.rating}</div>
+          <button>Add to Cart</button>
+        </div> 
+        </div>
+      
     </>
   );
 };
